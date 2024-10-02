2380a276ec9f0eb5085add0b4e1cf4ce972fe20c
ArianaT7 <148150898+ArianaT7@users.noreply.github.com>
2024-10-02 15:33:00 +0200

    Update and rename string.h to stringlib.h

---
+++ stringlib.h
@@ -0,0 +1,23 @@
+#ifndef STRINGLIB_H
+#define STRINGLIB_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    char* data;      
+    size_t length;   
+    size_t capacity; 
+} String;
+
+String* create_empty_string();
+String* create_string(const char *initial_data);
+void free_string(String* str);
+String* copy_string(const String *str);
+int compare_strings(const String* str1, const String* str2);
+String* concat_strings(const String *str1, const String *str2);
+void resize_string(String* str, size_t new_capacity);
+String* substring_string(const String *str, size_t start, size_t end);
+unsigned int hash_string(const String *str, int table_size);
+
+#endif
