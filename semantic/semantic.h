e64f6e8026fe8e12dab9d949b6f314fa04f47499
ArianaT7 <148150898+ArianaT7@users.noreply.github.com>
2024-11-02 10:41:26 +0100

    Update semantic.h

--- semantic/semantic.h
+++ semantic/semantic.h
@@ -5,5 +5,7 @@
 
 void semantic_scan(Node* parse_tree, SymbolTable* global_sym_table, String* global_func_key, SymbolTable* local_sym_table);
 bool find_ret_statement(Node* node);
+bool is_whole_float(double value);
+VarType semantic_expr(Node* node, SymbolTable* global_table, SymbolTable* local_table);
 
 #endif
